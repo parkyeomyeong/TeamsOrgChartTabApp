e38e2d9085f782ef5b6f486dff2550adca6a2c51
parkyeomyeong <tldldh1212@naver.com>
2026-02-04 17:57:14 +0900

    chore: 예전에 프로젝트 안에 서버추가하면서 설정한 것들 모두 지우기

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,11 @@ export default defineConfig({
   esbuild: {
     tsconfigRaw: fs.readFileSync("./tsconfig.app.json"),
   },
+  server: {
+    port: 53000,
+    https: {
+      key: fs.readFileSync(process.env.SSL_KEY_FILE),
+      cert: fs.readFileSync(process.env.SSL_CRT_FILE),
+    },
+  },
 });
